9777deb63a4b64d622dde844d35f7a672a40bc4f
Daniel <dfpires@gmail.com>
2026-04-24 21:56:57 -0300

    produto api

---
+++ produto/produto/src/main/java/fatecfranca/produto/service/ProdutoService.java
@@ -0,0 +1,24 @@
+package fatecfranca.produto.service;
+
+import fatecfranca.produto.model.Produto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProdutoService {
+    // cria a lista de produtos
+    private List<Produto> lista = new ArrayList<Produto>();
+    private Long nextId = 1L;
+
+    public List<Produto> listar(){
+        return this.lista;
+    }
+    public Produto criar(Produto p){
+        p.setId(nextId); // insere id
+        nextId++; // atualiza id
+        this.lista.add(p); // adiciona na lista
+        return p; // retorna produto
+    }
+}
